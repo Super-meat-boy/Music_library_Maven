2011f3e293866dc853e7381da5d1dbce17a23c5e
fabien terrasse <fabien.terrasse.pf1@gmail.com>
2018-04-10 17:28:48 +0200

    add Vinyl class and vinyl add function

---
+++ Vinyl.java
@@ -0,0 +1,29 @@
+public class Vinyl {
+
+
+    String album;
+    int year;
+
+    public Vinyl(String album, int year) {
+
+        this.album = album;
+        this.year = year;
+    }
+
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
